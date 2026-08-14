d9aa02120b5694ffde742d05b47b93419db3997f
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-08-15 02:27:45 +0530

    route bug fixed

--- src/services/careers.js
+++ src/services/careers.js
@@ -1,16 +1,16 @@
 import api from './api'
 
 export async function getCareers() {
-  const { data } = await api.get('/careers')
+  const { data } = await api.get('/api/careers')
   return data?.data?.careers || []
 }
 
 export async function getCareer(careerId) {
-  const { data } = await api.get(`/careers/${careerId}`)
+  const { data } = await api.get(`/api/careers/${careerId}`)
   return data?.data?.career || null
 }
 
 export async function getCareerSkillGaps(careerId) {
-  const { data } = await api.get(`/recommendations/careers/${careerId}/skill-gaps`)
+  const { data } = await api.get(`/api/recommendations/careers/${careerId}/skill-gaps`)
   return data?.data?.gaps || null
 }
