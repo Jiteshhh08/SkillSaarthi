2830174cc282aff21f30ef5199cfb0833034bb46
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-08-14 23:50:43 +0530

    Added routed for github and internship recomendation

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import healthRoutes from './routes/health.routes.js'
+import githubRoutes from './routes/github.routes.js'
+import internshipRoutes from './routes/internship.routes.js'
 import { errorHandler, notFound } from './middleware/error.middleware.js'
 
 const app = express()
@@ -9,6 +11,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api', healthRoutes)
+app.use('/api/github', githubRoutes)
+app.use('/api/internships', internshipRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
