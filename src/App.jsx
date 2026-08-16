3f403570a268f066a8c5f0fb6a5e05b79ae808d4
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-08-16 20:52:22 +0530

    Added a scroll to top Function for better UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import AppRoutes from './routes/AppRoutes'
+import ScrollToTop from './components/common/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
